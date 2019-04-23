a9407705a12ef6fa6ee7652d1e84c24db6a7e0fa
Roy <roy071995@gmail.com>
2019-04-23 20:36:10 +0200

    Allemal zeik uit bestand verwijderd
    Geen idee waar dat vandaan komt

--- bluetoothRCCar.ino
+++ bluetoothRCCar.ino
@@ -2,13 +2,8 @@
 
 #define headLightL A3
 #define headLightR A2
-<<<<<<< HEAD
 #define rearLightL 16
 #define rearLightR 14
-=======
-#define rearLightL 14
-#define rearLightR 16
->>>>>>> master
 
 #define steerL A1
 #define steerR A0
@@ -16,11 +11,7 @@
 #define driveF 5
 #define driveR 6
 
-<<<<<<< HEAD
 #define bluetooth Serial1
-=======
-#define bluetooth Serial1;
->>>>>>> master
 
 //data vb: M255,R0,S0,L1,Ih
 //M(otor):     pwm value (0 - 255)
@@ -28,16 +19,10 @@
 //S(teering):  l(eft), 0 (center), r(ight)
 //L(ight):     1 (on), 0 (off)
 //I(ndicator): l(eft indicator), r(ight indicator), h(azard lights), o(ff)
-<<<<<<< HEAD
 //C(onnect):   respond with hello
 String dataIn;
 
 /*enum steerDir { Center, Left, Right };
-=======
-/*String dataIn;
-
-enum steerDir { Center, Left, Right };
->>>>>>> master
 typedef struct btData{
   byte motor;
   bool motorDir;
@@ -52,11 +37,9 @@ void setup() {
 
   pinMode(headLightL, OUTPUT);
   pinMode(headLightR, OUTPUT);
-<<<<<<< HEAD
+  
   pinMode(rearLightL, OUTPUT);
   pinMode(rearLightR, OUTPUT);
-=======
->>>>>>> master
   /*pinMode(steerL, OUTPUT);
   pinMode(steerR, OUTPUT);
   digitalWrite(steerL, LOW);
@@ -64,16 +47,11 @@ void setup() {
 
   xTaskCreate(
     ledTask,
-<<<<<<< HEAD
     "Blink",
-=======
-    (const portCHAR *)"Blink",
->>>>>>> master
     128,  // Stack size
     NULL,
     2,  // priority
     NULL);
-<<<<<<< HEAD
 
   xTaskCreate(
     btTask,
@@ -82,8 +60,6 @@ void setup() {
     NULL,
     3,
     NULL);
-=======
->>>>>>> master
 }
 
 void loop() {
@@ -92,7 +68,6 @@ void loop() {
 
 void ledTask(void *params) {
   while (true) {
-<<<<<<< HEAD
     digitalWrite(headLightL, HIGH);
     digitalWrite(headLightR, HIGH);
     digitalWrite(rearLightL, HIGH);
@@ -110,37 +85,23 @@ void btTask(void *param) {
   while (true) {
     checkData();
     vTaskDelay(50/portTICK_PERIOD_MS);
-=======
-    digitalWrite(headLightL, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(headLightL, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
->>>>>>> master
   }
 }
 
 void checkData() {
   while (bluetooth.available()) {
-<<<<<<< HEAD
     char c = (char)bluetooth.read();
-=======
-    char c = bluetooth.read();
->>>>>>> master
     if (c == '\n') {
       parseData();
       dataIn = "";
     }
-<<<<<<< HEAD
     else
       dataIn += c;
-=======
->>>>>>> master
   }
 }
 
 void parseData() {
   String tmp;
-<<<<<<< HEAD
   bool last = false;
   while (!last) {
     if (dataIn.indexOf(',') != -1) {
@@ -172,10 +133,5 @@ void parseData() {
         break;
     }
     dataIn.remove(0, tmp.length() + 1);
-=======
-  while (1) {
-    if (dataIn.indexOf(",") != -1)
-      tmp = dataIn.substring(0, dataIn.indexOf(","));
->>>>>>> master
   }
 }
