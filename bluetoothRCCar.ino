15b9fdb7fc5f767f969fb5a6d2440c5691af7545
Roy <roy071995@gmail.com>
2019-04-23 20:27:30 +0200

    Added Bluetooth task

--- bluetoothRCCar.ino
+++ bluetoothRCCar.ino
@@ -2,8 +2,8 @@
 
 #define headLightL A3
 #define headLightR A2
-#define rearLightL 14
-#define rearLightR 16
+#define rearLightL 16
+#define rearLightR 14
 
 #define steerL A1
 #define steerR A0
@@ -11,7 +11,7 @@
 #define driveF 5
 #define driveR 6
 
-#define bluetooth Serial1;
+#define bluetooth Serial1
 
 //data vb: M255,R0,S0,L1,Ih
 //M(otor):     pwm value (0 - 255)
@@ -19,9 +19,10 @@
 //S(teering):  l(eft), 0 (center), r(ight)
 //L(ight):     1 (on), 0 (off)
 //I(ndicator): l(eft indicator), r(ight indicator), h(azard lights), o(ff)
-/*String dataIn;
+//C(onnect):   respond with hello
+String dataIn;
 
-enum steerDir { Center, Left, Right };
+/*enum steerDir { Center, Left, Right };
 typedef struct btData{
   byte motor;
   bool motorDir;
@@ -36,6 +37,8 @@ void setup() {
 
   pinMode(headLightL, OUTPUT);
   pinMode(headLightR, OUTPUT);
+  pinMode(rearLightL, OUTPUT);
+  pinMode(rearLightR, OUTPUT);
   /*pinMode(steerL, OUTPUT);
   pinMode(steerR, OUTPUT);
   digitalWrite(steerL, LOW);
@@ -43,10 +46,18 @@ void setup() {
 
   xTaskCreate(
     ledTask,
-    (const portCHAR *)"Blink",
+    "Blink",
     128,  // Stack size
     NULL,
     2,  // priority
+    NULL);
+
+  xTaskCreate(
+    btTask,
+    "Bluetooth",
+    256,
+    NULL,
+    3,
     NULL);
 }
 
@@ -56,27 +67,70 @@ void loop() {
 
 void ledTask(void *params) {
   while (true) {
-    digitalWrite(headLightL, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(headLightL, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    digitalWrite(headLightL, HIGH);
+    digitalWrite(headLightR, HIGH);
+    digitalWrite(rearLightL, HIGH);
+    digitalWrite(rearLightR, HIGH);
+    vTaskDelay( 500/portTICK_PERIOD_MS );
+    digitalWrite(headLightL, LOW);
+    digitalWrite(headLightR, LOW);
+    digitalWrite(rearLightL, LOW);
+    digitalWrite(rearLightR, LOW);
+    vTaskDelay( 500/portTICK_PERIOD_MS );
+  }
+}
+
+void btTask(void *param) {
+  while (true) {
+    checkData();
+    vTaskDelay(50/portTICK_PERIOD_MS);
   }
 }
 
 void checkData() {
   while (bluetooth.available()) {
-    char c = bluetooth.read();
+    char c = (char)bluetooth.read();
     if (c == '\n') {
       parseData();
       dataIn = "";
     }
+    else
+      dataIn += c;
   }
 }
 
 void parseData() {
   String tmp;
-  while (1) {
-    if (dataIn.indexOf(",") != -1)
-      tmp = dataIn.substring(0, dataIn.indexOf(","));
+  bool last = false;
+  while (!last) {
+    if (dataIn.indexOf(',') != -1) {
+      tmp = dataIn.substring(0, dataIn.indexOf(','));
+      Serial.println(tmp);
+    }
+    else {
+      tmp = dataIn;
+      last = true;
+    }
+    
+    Serial.println("tmp: " + tmp + "\nrest: " + dataIn);
+    char c = tmp.charAt(0);
+    tmp.remove(0, 1);
+    switch(c) {
+      case 'M': //Motor
+        break;
+      case 'R': //Reverse
+        break;
+      case 'S': //Steering
+        break;
+      case 'L': //Lights
+        break;
+      case 'I': //Indicator
+        break;
+      case 'C': //Connect
+        bluetooth.println("hello");
+      default:
+        break;
+    }
+    dataIn.remove(0, tmp.length() + 1);
   }
 }
