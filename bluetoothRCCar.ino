4b46efc19ec8a7fbd97f7484b442bcc613d4c424
Roy <roy071995@gmail.com>
2019-04-28 20:43:21 +0200

    Working version using tasks and stuff
    Motor not implemented yet

--- bluetoothRCCar.ino
+++ bluetoothRCCar.ino
@@ -1,17 +1,21 @@
 #include <Arduino_FreeRTOS.h>
+#include <timers.h>
+#include <queue.h>
 
-#define headLightL A3
-#define headLightR A2
+#define headLightL A2
+#define headLightR A3
 #define rearLightL 16
 #define rearLightR 14
 
 #define steerL A1
 #define steerR A0
 
-#define driveF 5
-#define driveR 6
+#define motorF 5
+#define motorR 6
 
-#define bluetooth Serial1
+#define inputDataBuffer 32
+#define bluetooth Serial
+#define debug Serial
 
 //data vb: M255,R0,S0,L1,Ih
 //M(otor):     pwm value (0 - 255)
@@ -19,119 +23,296 @@
 //S(teering):  l(eft), 0 (center), r(ight)
 //L(ight):     1 (on), 0 (off)
 //I(ndicator): l(eft indicator), r(ight indicator), h(azard lights), o(ff)
-//C(onnect):   respond with hello
-String dataIn;
 
-/*enum steerDir { Center, Left, Right };
-typedef struct btData{
-  byte motor;
-  bool motorDir;
-  int steer;
-  byte lights;
-};*/
+typedef struct {
+  byte speed;
+  bool direction;
+} motor_t;
+motor_t motorData = {0, 0};
+
+#define steerCenter 0
+#define steerRight  1
+#define steerLeft   2
+byte steerData= 0;
+
+#define lightStateBit  (1 << 0)
+#define indicatorLBit  (1 << 1)
+#define indicatorRBit  (1 << 2)
+#define hazardBit      (1 << 3)
+#define indicatorState (1 << 4)
+byte lightData = 0;
+
+TaskHandle_t btTaskHandle = NULL;
+TaskHandle_t motorTaskHandle = NULL;
+TaskHandle_t steerTaskHandle = NULL;
+TaskHandle_t lightTaskHandle = NULL;
+
+TimerHandle_t indicatorTimer = NULL;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial1.begin(9600);
+  Serial.begin(115200); //USB debug
+  Serial1.begin(9600);  //bluetooth module
 
   pinMode(headLightL, OUTPUT);
   pinMode(headLightR, OUTPUT);
-  
   pinMode(rearLightL, OUTPUT);
   pinMode(rearLightR, OUTPUT);
-  /*pinMode(steerL, OUTPUT);
+  pinMode(steerL, OUTPUT);
   pinMode(steerR, OUTPUT);
-  digitalWrite(steerL, LOW);
-  digitalWrite(steerR, LOW);*/
+  pinMode(motorF, OUTPUT);
+  pinMode(motorR, OUTPUT);
 
   xTaskCreate(
-    ledTask,
-    "Blink",
-    128,  // Stack size
-    NULL,
-    2,  // priority
-    NULL);
+    btTask, //task
+    "Bluetooth", //task name
+    512, //stack size
+    NULL, //parameters
+    3, //priority
+    &btTaskHandle //task handle
+  );
 
   xTaskCreate(
-    btTask,
-    "Bluetooth",
+    motorTask,
+    "Motor",
     256,
     NULL,
-    3,
-    NULL);
+    2,
+    &motorTaskHandle
+  );
+
+  xTaskCreate(
+    steerTask,
+    "Steer",
+    256,
+    NULL,
+    2,
+    &steerTaskHandle
+  );
+  
+  xTaskCreate(
+    lightTask,
+    "Light",
+    256,
+    NULL,
+    1,
+    &lightTaskHandle
+  );
+  
+  indicatorTimer = xTimerCreate(
+    "indicator timer", //timer name
+    500/portTICK_PERIOD_MS, //timer period
+    pdTRUE, //autoreload
+    NULL, //timer ID
+    indicatorTimerTick //callback function
+  );
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-void ledTask(void *params) {
+void motorTask(void *params) {
   while (true) {
-    digitalWrite(headLightL, HIGH);
-    digitalWrite(headLightR, HIGH);
-    digitalWrite(rearLightL, HIGH);
-    digitalWrite(rearLightR, HIGH);
-    vTaskDelay( 500/portTICK_PERIOD_MS );
-    digitalWrite(headLightL, LOW);
-    digitalWrite(headLightR, LOW);
-    digitalWrite(rearLightL, LOW);
-    digitalWrite(rearLightR, LOW);
-    vTaskDelay( 500/portTICK_PERIOD_MS );
+    if (ulTaskNotifyTake(pdTRUE, portMAX_DELAY)) {
+#ifdef debug
+      debug.print("Motor: ");
+      debug.print(motorData.speed);
+      debug.print(", reverse: ");
+      debug.println(motorData.direction);
+#endif
+      /*if(motorData.direction) {
+        analogWrite(driveF, 0);
+        analogWrite(driveR, motorData.speed);
+      } else {
+        analogWrite(driveF, motorData.speed);
+        analogWrite(driveR, 0);
+      }*/
+    }
   }
+}
+
+void steerTask(void *params) {
+  while (true) {
+    if (ulTaskNotifyTake(pdTRUE, portMAX_DELAY)) {
+#ifdef debug
+      debug.print("Steer: 0x");
+      debug.println(steerData, HEX);
+#endif
+      if (steerData == steerLeft) {
+        digitalWrite(steerL, HIGH);
+        digitalWrite(steerR, LOW);
+      }
+      else if (steerData == steerRight) {
+        digitalWrite(steerL, LOW);
+        digitalWrite(steerR, HIGH);
+      }
+      else {
+        digitalWrite(steerL, LOW);
+        digitalWrite(steerR, LOW);
+      }
+    }
+  }
+}
+
+void lightTask(void *params) {
+  bool blink = false;
+  
+  while (true) {
+    if (ulTaskNotifyTake(pdTRUE, portMAX_DELAY)) {
+      //handle indicators
+      blink = false;
+      if (lightData & (indicatorLBit | hazardBit)) { //left indicator
+        blink = true;
+      }
+      else {
+        digitalWrite(headLightL, (lightData & lightStateBit) != 0);
+        digitalWrite(rearLightL, (lightData & lightStateBit) != 0);
+      }
+      
+      if (lightData & (indicatorRBit | hazardBit)) { //right indicator
+        blink = true;
+      }
+      else {
+        digitalWrite(headLightR, (lightData & lightStateBit) != 0);
+        digitalWrite(rearLightR, (lightData & lightStateBit) != 0);
+      }
+      
+      if (blink) {
+        if (!xTimerIsTimerActive(indicatorTimer)) { //start timer if not active
+          if ((lightData & lightStateBit) != 0)
+            lightData |= indicatorState; //turn on indicatorstatebit to immediately toggle indicator light when headlights are on
+          else
+            lightData &= ~indicatorState;
+          
+          indicatorTimerTick(indicatorTimer); //call first time for immediate response
+          if (!xTimerStart(indicatorTimer, 0)) {
+          }
+        }
+      }
+      else { //indicators not used
+        if (!xTimerStop(indicatorTimer, 0)) {
+        }
+      }
+    }
+  }
+}
+
+void indicatorTimerTick(TimerHandle_t tmr) {
+  if ((lightData & indicatorLBit) | (lightData & hazardBit)) {
+    digitalWrite(headLightL, (lightData & indicatorState) == 0);
+    digitalWrite(rearLightL, (lightData & indicatorState) == 0);
+  }
+  if ((lightData & indicatorRBit) | (lightData & hazardBit)) {
+    digitalWrite(headLightR, (lightData & indicatorState) == 0);
+    digitalWrite(rearLightR, (lightData & indicatorState) == 0);
+  }
+  lightData ^= indicatorState; //toggle state of indicator bit
 }
 
 void btTask(void *param) {
+  char dataIn[inputDataBuffer + 1] = {'\0'};
+  unsigned long lastMsgTime = 0;
+  
   while (true) {
-    checkData();
-    vTaskDelay(50/portTICK_PERIOD_MS);
+    checkData(dataIn);
+    vTaskDelay(200/portTICK_PERIOD_MS);
   }
 }
 
-void checkData() {
+void checkData(char* dataIn) {
   while (bluetooth.available()) {
     char c = (char)bluetooth.read();
     if (c == '\n') {
-      parseData();
-      dataIn = "";
-    }
-    else
-      dataIn += c;
-  }
-}
-
-void parseData() {
-  String tmp;
-  bool last = false;
-  while (!last) {
-    if (dataIn.indexOf(',') != -1) {
-      tmp = dataIn.substring(0, dataIn.indexOf(','));
-      Serial.println(tmp);
+      parseData(dataIn);
+      memset(dataIn, '\0', inputDataBuffer + 1);
     }
     else {
-      tmp = dataIn;
-      last = true;
+      if (strlen(dataIn) < inputDataBuffer)
+        dataIn[strlen(dataIn)] = c;
+      else
+        dataIn[inputDataBuffer] = c;
     }
+  }
+  return dataIn;
+}
+
+void parseData(char* dataIn) {
+  char tmp[8] = {'\0'};
+  
+  while (true) {
+    memset(tmp, '\0', sizeof(tmp));
+    int len = sizeof(tmp) - 1; 
+    if (strchr(dataIn, ',') != NULL) {
+      int newLen = strchr(dataIn, ',') - dataIn;
+      if (newLen < len)
+        len = newLen;
+    }
+    strncpy(tmp, dataIn, len); //copy data until comma to tmp
     
-    Serial.println("tmp: " + tmp + "\nrest: " + dataIn);
-    char c = tmp.charAt(0);
-    tmp.remove(0, 1);
-    switch(c) {
+    switch(tmp[0]) {
       case 'M': //Motor
+        motorData.speed = atoi(&tmp[1]);
+        xTaskNotifyGive(motorTaskHandle);
         break;
+        
       case 'R': //Reverse
+        motorData.direction = (tmp[1] == '1');
+        xTaskNotifyGive(motorTaskHandle);
         break;
+        
       case 'S': //Steering
+        switch (tmp[1]) {
+          case 'l':
+            steerData = steerLeft;
+            break;
+          case 'r':
+            steerData = steerRight;
+            break;
+          default: //0 or unknown value
+            steerData = steerCenter;
+            break;
+        }
+        xTaskNotifyGive(steerTaskHandle);
         break;
+        
       case 'L': //Lights
+        if (tmp[1] == '1') //toggle lights
+          lightData ^= lightStateBit;
+        else //0 or unknown value: lights off
+          lightData &= ~lightStateBit;
+        
+        xTaskNotifyGive(lightTaskHandle);
         break;
+        
       case 'I': //Indicator
+        switch (tmp[1]) {
+          case 'l': //toggle left indicator
+            lightData ^= indicatorLBit;
+            lightData &= ~indicatorRBit;
+            break;
+          case 'r': //togle right indicator
+            lightData ^= indicatorRBit;
+            lightData &= ~indicatorLBit;
+            break;
+          case 'h': //toggle hazard light
+            lightData ^= hazardBit;
+            break;
+          default: //0 or unknown value: all off
+            lightData &= ~(indicatorLBit | indicatorRBit | hazardBit);
+            break;
+        }
+        xTaskNotifyGive(lightTaskHandle);
         break;
-      case 'C': //Connect
-        bluetooth.println("hello");
+        
       default:
         break;
     }
-    dataIn.remove(0, tmp.length() + 1);
+    
+    dataIn = strchr(dataIn, ','); //set the dataIn pointer after the comma
+    if (dataIn != NULL)
+      dataIn++;
+    else 
+      break;
   }
 }
